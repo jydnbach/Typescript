c16f1c6e55933c4dbb9cc291e788820a11e6ab72
jydnbach <jaydenkim95@gmail.com>
2024-07-09 10:31:59 +0900

    when to use annotaions: delayed initialization

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -36,4 +36,14 @@ const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // {x: 10, y: 20};
 
-// 2.
+// 2. when we declare a vriable on one line and initializate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3.
