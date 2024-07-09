b10e2202deba5df55d03fc6bfd4007e7091834c8
jydnbach <jaydenkim95@gmail.com>
2024-07-09 10:35:41 +0900

    when to use annotation: when inference doesn't work

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -46,4 +46,12 @@ for (let i = 0; i < words.length; i++) {
   }
 }
 
-// 3.
+// 3. variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // or syntax
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
