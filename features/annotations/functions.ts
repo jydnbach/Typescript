defeeadd45b2e129d484f7b93a2901fdb212a531
jydnbach <jaydenkim95@gmail.com>
2024-07-09 10:53:08 +0900

    void and never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -6,6 +6,7 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number) => {};
 // void
 
+//// anoynymous function ////
 function divide(a: number, b: number): number {
   return a / b;
 }
@@ -13,3 +14,14 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+//// void and never ////
+const logger = (message: string): void => {
+  console.log(message);
+};
+// it's not gonna return anything
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+//////////
