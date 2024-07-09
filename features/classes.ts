d9fb414e97f1b2d6799040bedfb656aba0f13b06
jydnbach <jaydenkim95@gmail.com>
2024-07-09 16:18:45 +0900

    fields in classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,29 +1,33 @@
 class Vehicle {
+  //initial function
+  constructor(public color: string) {} // public modifier is a shortcut for boilerplate
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle('orange');
 // vehicle.honk(); // not allowed bc honk is protected
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    // private doesn't mean security. it restricts developers to call it
-    console.log('vroom'); // overrides Vehicle
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     // private doesn't mean security. it restricts developers to call it
+//     console.log('vroom'); // overrides Vehicle
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    // this.honk(); // not allowed
-    this.honk; // thanks to protected
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     // this.honk(); // not allowed
+//     this.honk; // thanks to protected
+//   }
+// }
 
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
+// // const vehicle = new Vehicle();
+// // vehicle.drive();
+// // vehicle.honk();
 
-const car2 = new Car();
-car2.startDrivingProcess();
-// car2.honk();
+// const car2 = new Car();
+// car2.startDrivingProcess();
+// // car2.honk();
