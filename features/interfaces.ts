d071f6411990594093ea7618ae971210a9dfa489
jydnbach <jaydenkim95@gmail.com>
2024-07-09 15:22:45 +0900

    interfaces

---
+++ features/interfaces.ts
@@ -0,0 +1,19 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true, // e.g. isBroken not accepted
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken? ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
