42228a71ee5cdf8ce5edcb9c30886221b3c23b3c
jydnbach <jaydenkim95@gmail.com>
2024-07-09 09:54:21 +0900

    object literal annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -6,3 +6,18 @@ let nothingMuch: null = null;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
