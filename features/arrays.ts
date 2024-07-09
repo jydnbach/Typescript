3e4ccc19e8a83cd7c25778d2bf154e705a61a5c6
jydnbach <jaydenkim95@gmail.com>
2024-07-09 14:49:36 +0900

    typed arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -2,3 +2,15 @@ const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const cardsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+// help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
