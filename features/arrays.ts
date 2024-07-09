7d0579abcaa601a89dce60ac1063637206f73f35
jydnbach <jaydenkim95@gmail.com>
2024-07-09 14:52:39 +0900

    multiple types in arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -8,9 +8,14 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // Prevent incompatible values
-carMakers.push(100);
+// carMakers.push(100);
 
 // help with 'map'
 carMakers.map((car: string): string => {
   return car.toUpperCase();
 });
+
+// flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
